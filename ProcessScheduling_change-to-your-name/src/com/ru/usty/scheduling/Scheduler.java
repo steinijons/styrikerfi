13c2d5f33b1b48cd9e869461fabd4e1509ac3cd0
Steini <hivsteini@gmail.com>
2017-03-08 10:54:56 +0000

    initial commit fyrir processSceduling

---
+++ ProcessScheduling_change-to-your-name/src/com/ru/usty/scheduling/Scheduler.java
@@ -0,0 +1,105 @@
+package com.ru.usty.scheduling;
+
+import com.ru.usty.scheduling.process.ProcessExecution;
+
+public class Scheduler {
+
+	ProcessExecution processExecution;
+	Policy policy;
+	int quantum;
+
+	/**
+	 * Add any objects and variables here (if needed)
+	 */
+
+
+	/**
+	 * DO NOT CHANGE DEFINITION OF OPERATION
+	 */
+	public Scheduler(ProcessExecution processExecution) {
+		this.processExecution = processExecution;
+
+		/**
+		 * Add general initialization code here (if needed)
+		 */
+	}
+
+	/**
+	 * DO NOT CHANGE DEFINITION OF OPERATION
+	 */
+	public void startScheduling(Policy policy, int quantum) {
+
+		this.policy = policy;
+		this.quantum = quantum;
+
+		/**
+		 * Add general initialization code here (if needed)
+		 */
+
+		switch(policy) {
+		case FCFS:	//First-come-first-served
+			System.out.println("Starting new scheduling task: First-come-first-served");
+			/**
+			 * Add your policy specific initialization code here (if needed)
+			 */
+			break;
+		case RR:	//Round robin
+			System.out.println("Starting new scheduling task: Round robin, quantum = " + quantum);
+			/**
+			 * Add your policy specific initialization code here (if needed)
+			 */
+			break;
+		case SPN:	//Shortest process next
+			System.out.println("Starting new scheduling task: Shortest process next");
+			/**
+			 * Add your policy specific initialization code here (if needed)
+			 */
+			break;
+		case SRT:	//Shortest remaining time
+			System.out.println("Starting new scheduling task: Shortest remaining time");
+			/**
+			 * Add your policy specific initialization code here (if needed)
+			 */
+			break;
+		case HRRN:	//Highest response ratio next
+			System.out.println("Starting new scheduling task: Highest response ratio next");
+			/**
+			 * Add your policy specific initialization code here (if needed)
+			 */
+			break;
+		case FB:	//Feedback
+			System.out.println("Starting new scheduling task: Feedback, quantum = " + quantum);
+			/**
+			 * Add your policy specific initialization code here (if needed)
+			 */
+			break;
+		}
+
+		/**
+		 * Add general scheduling or initialization code here (if needed)
+		 */
+
+	}
+
+	/**
+	 * DO NOT CHANGE DEFINITION OF OPERATION
+	 */
+	public void processAdded(int processID) {
+
+		/**
+		 * Add scheduling code here
+		 */
+
+	}
+
+	/**
+	 * DO NOT CHANGE DEFINITION OF OPERATION
+	 */
+	public void processFinished(int processID) {
+
+		/**
+		 * Add scheduling code here
+		 */
+
+	}
+}
