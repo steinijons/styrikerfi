26b7cd9fa4624392c2e26146dccd7545ff3f658c
Steini <hivsteini@gmail.com>
2017-03-14 09:52:38 +0000

    addað processData

---
+++ ProcessScheduling_change-to-your-name/src/com/ru/usty/scheduling/ProcessData.java
@@ -0,0 +1,11 @@
+package com.ru.usty.scheduling;
+
+public class ProcessData {
+	int processID;
+	long someTime;
+	public ProcessData(int processID, long someTime)
+	{
+		this.processID = processID;
+		this.someTime = someTime;
+	}
+}
